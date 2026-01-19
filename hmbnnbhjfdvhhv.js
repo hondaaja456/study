a014e75c5d74973703f0c58fa99ddc3e997b8d51
hondaaja456 <honda456yuni77@gmail.com>
2026-01-19 18:16:09 +0800

    Create hmbnnbhjfdvhhv.js

---
+++ hmbnnbhjfdvhhv.js
@@ -0,0 +1,10 @@
+function escapeHTML(str) {
+  return str.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+}
+
+let name = prompt("Enter your character's name:");
+document.getElementById("charName").innerHTML = escapeHTML(name);
